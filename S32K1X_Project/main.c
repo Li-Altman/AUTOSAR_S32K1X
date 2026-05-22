27eb1ca2be6024dab0791a69b28a0d6e414897d5
Li-Altman <yunfeili_tju@outlook.com>
2026-05-22 22:45:49 +0800

    1. Init the AUTOSAR S32K1X project

---
+++ S32K1X_Project/main.c
@@ -0,0 +1,12 @@
+/*
+ * @Author: L_TGo yunfei_li@tju.edu.cn
+ * @Date: 2026-05-22 22:17:52
+ * @LastEditors: L_TGo yunfei_li@tju.edu.cn
+ * @LastEditTime: 2026-05-22 22:23:23
+ * @FilePath: \S32K1x_WorkSpace\main.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+int main(void) {
+    return 0;
+}
